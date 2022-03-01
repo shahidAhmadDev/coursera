cf524ad924652beba6d289514278e77a459fc73e
shahidAhmadDev <90954954+shahidAhmadDev@users.noreply.github.com>
2022-03-01 14:51:03 +0530

    Add files via upload
    module4 assignment solution

---
+++ module4/js/script.js
@@ -0,0 +1,9 @@
+var names = ["yakov","john","jimmy","shahid","farooq","larry","paul","loura","muskaan","jahid"];
+for(var i = 0;i<names.length;i++){
+    if(names[i].charAt(0)=='j' || names[i].charAt(0)=='J'){
+        console.log("Goodbye "+ names[i]);
+    }else{
+        console.log("Hello "+names[i]);
+    }
+
+}
